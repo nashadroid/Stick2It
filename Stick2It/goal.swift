e379a3143098a4168d792c1f891e539911316716
Nashad <rahman.176@osu.edu>
2020-05-04 12:49:05 -0400

    added struct: goal

---
+++ Stick2It/goal.swift
@@ -0,0 +1,24 @@
+//
+//  goal.swift
+//  Stick2It
+//
+//  Created by Nashad Rahman on 5/4/20.
+//  Copyright © 2020 NashApps. All rights reserved.
+//
+
+import SwiftUI
+
+//TODO: Change id to customized
+//TODO: Try Default Values
+
+struct Goal: Hashable {
+    var id = UUID()
+    var goalName: String = "sampleGoal"
+    var done: Bool = false
+    
+    mutating func updateDone(){
+        self.done = true
+    }
+    
+}
+
