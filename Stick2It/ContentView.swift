1ccb574771a0d8318ce15b1ed2446e2fb79f91a5
Nashad <rahman.176@osu.edu>
2020-05-04 11:52:29 -0400

    Initial Commit

---
+++ Stick2It/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stick2It
+//
+//  Created by Nashad Rahman on 5/4/20.
+//  Copyright © 2020 NashApps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
