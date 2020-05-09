c94959ed7fb061948a73a56402344a755c52ab2f
Nashad <rahman.176@osu.edu>
2020-05-08 22:19:39 -0400

    Added Tab View and linked to routines. Annoying dark mode at it again

--- Stick2It/TodayView.swift
+++ Stick2It/TodayView.swift
@@ -11,31 +11,65 @@ import SwiftUI
 struct TodayView: View {
     @EnvironmentObject var userData: UserData
     @State var date: String = "none"
+    @State var addingItem: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0){
-            Text("Today's Goals")
-                .foregroundColor(Color.black)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .padding()
-                .padding(.leading, 10)
-                .multilineTextAlignment(.leading)
-            ScrollView(.vertical, showsIndicators: false){
-                VStack(spacing: 10){
-                    
-                    ForEach(userData.userGoals) {goal in
-                        if self.date == goal.date{
-                            GoalBox(goal: goal)
+        ZStack{
+            VStack(alignment: .leading, spacing: 0){
+                Text("Today's Goals")
+                    .foregroundColor(Color.black)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .padding()
+                    .padding(.leading, 10)
+                    .multilineTextAlignment(.leading)
+                ScrollView(.vertical, showsIndicators: false){
+                    VStack(spacing: 10){
+                        
+                        ForEach(userData.userGoals) {goal in
+                            if self.date == goal.date{
+                                GoalBox(goal: goal)
+                            }
                         }
+                        //
+                        //                        Button(action: {self.addingItem.toggle()}) {
+                        //                            Text("Add")
+                        //                        }
                     }
-                    //
-                    //                        Button(action: {self.addingItem.toggle()}) {
-                    //                            Text("Add")
-                    //                        }
+                    .padding()
+                    .frame(minWidth: UIScreen.main.bounds.size.width)
                 }
-                .padding()
-                .frame(minWidth: UIScreen.main.bounds.size.width)
+                
+            }
+            
+            if(addingItem){
+                GeometryReader{_ in
+                    BlurView(style: .light)
+                        .onTapGesture {
+                            self.addingItem.toggle()
+                    }
+                    
+                    AddItemNoBack(userData: self._userData, addingItem: self.addingItem)
+                        .padding(.top, 40)
+                    
+                }.background(
+                    Color.black.opacity(0.65)
+                        .edgesIgnoringSafeArea(.all)
+                        .onTapGesture {
+                            self.addingItem.toggle()
+                    }
+                )
+                    .edgesIgnoringSafeArea(.all)
+            }
+            else{
+                Button(action: {self.addingItem.toggle()}) {
+                    AddButton()
+                }
+                .scaleEffect(0.2)
+                .offset(x: 120, y: 320)
+                //TODO: This needs to be adjusted to work with all screen sizes
+                
+                
             }
             
         }
